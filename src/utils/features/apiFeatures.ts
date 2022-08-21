2426fdf55018500e497e35b860ce28ed07748849
Sally-Builds <uzoagulujoshua@yahoo.com>
2022-08-21 08:45:03 +0100

    apiFeature added to utils/features folder readme.md - Postman documetation link added

---
+++ src/utils/features/apiFeatures.ts
@@ -0,0 +1,56 @@
+class APIFeatures {
+    private query
+    private queryString
+    constructor(query:any, queryString:any) {
+      this.query = query;
+      this.queryString = queryString;
+    }
+  
+    filter() {
+      // eslint-disable-next-line node/no-unsupported-features/es-syntax
+      const queryObj = { ...this.queryString };
+      const excludedFields = ['page', 'sort', 'limit', 'fields'];
+      excludedFields.forEach((el) => delete queryObj[el]);
+  
+      let queryStr = JSON.stringify(queryObj);
+      queryStr = queryStr.replace(
+        /\b(gte|lt|lte|gt|eq|ne|nin)\b/g,
+        (match) => `$${match}`
+      );
+  
+      this.query = this.query.find(JSON.parse(queryStr));
+      return this;
+    }
+  
+    sort() {
+      if (this.queryString.sort) {
+        const sortBy = this.queryString.sort.split(',').join(' ');
+        this.query = this.query.sort(sortBy);
+      } else {
+        this.query = this.query.sort('-createdAt');
+      }
+      return this;
+    }
+  
+    limitFields() {
+      if (this.queryString.fields) {
+        const fields = this.queryString.fields.split(',').join(' ');
+        this.query = this.query.select(fields);
+      } else {
+        this.query = this.query.select('-__v');
+      }
+      return this;
+    }
+  
+    paginate() {
+      const pg = this.queryString.page * 1 || 1;
+      const lim = this.queryString.limit * 1 || 100;
+      const skip = (pg - 1) * lim;
+      this.query = this.query.skip(skip).limit(lim);
+  
+      return this;
+    }
+  }
+  
+export default APIFeatures;
+  
